33d95d221ceea6cc5a61c9cd9ed3e12fe6aa753d
Yannick Scherer <yannick.scherer@gmail.com>
2013-12-14 12:54:28 +0100

    upgraded dependencies, using ':pedantic?'.

--- project.clj
+++ project.clj
@@ -5,13 +5,15 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories  {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.reader "0.7.10"]
+                 [org.clojure/tools.reader "0.8.2"]
                  [fast-zip "0.3.0"]
                  [potemkin "0.3.4"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]
-                   :plugins [[lein-midje "3.1.3-RC2"]]}
+  :profiles {:dev {:dependencies [[midje "1.6.0" :exclusions [joda-time]]
+                                  [joda-time "2.3"]]
+                   :plugins [[lein-midje "3.1.3"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
   :aliases {"midje-all" ["with-profile" "dev,1.4:dev,1.5:dev,1.6" "midje"]
-            "deps-all" ["with-profile" "dev,1.4:dev,1.5:dev,1.6" "deps"]})
+            "deps-all" ["with-profile" "dev,1.4:dev,1.5:dev,1.6" "deps"]}
+  :pedantic? :abort)
