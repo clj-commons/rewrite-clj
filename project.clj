24a1ce1a4a8e2278c2647309a44ea10ca1aa30a3
Yannick Scherer <yannick.scherer@stylefruits.de>
2014-11-23 13:46:00 +0100

    drop support for Clojure 1.4

--- project.clj
+++ project.clj
@@ -12,9 +12,8 @@
                                   [joda-time "2.5"]]
                    :plugins [[lein-midje "3.1.3"]]
                    :exclusions [org.clojure/clojure]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
-  :aliases {"all" ["with-profile" "dev,1.4:dev,1.5:dev,1.6"]
-            "test-ancient" ["with-profile" "dev,1.4:dev,1.5:dev,1.6" "midje"]}
+  :aliases {"all" ["with-profile" "dev,1.5:dev,1.6"]
+            "test-ancient" ["with-profile" "dev,1.5:dev,1.6" "midje"]}
   :pedantic? :abort)
