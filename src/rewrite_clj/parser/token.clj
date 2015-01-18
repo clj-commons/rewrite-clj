5fd44e4f2ab43361df89a8b8fb855439597014da
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-01-18 09:17:24 +0100

    fix reading of '\' character (closes #17).

--- src/rewrite_clj/parser/token.clj
+++ src/rewrite_clj/parser/token.clj
@@ -3,13 +3,27 @@
              [node :as node]
              [reader :as r]]))
 
+(defn- read-to-boundary
+  [reader]
+  (r/read-until
+    reader
+    r/whitespace-or-boundary?))
+
+(defn- read-to-char-boundary
+  [reader]
+  (let [c (r/next reader)]
+    (str c
+         (if (not= c \\)
+           (read-to-boundary reader)
+           ""))))
+
 (defn parse-token
   "Parse a single token."
   [reader]
-  (let [s (str
-            (r/next reader)
-            (r/read-until
-              reader
-              r/whitespace-or-boundary?))
+  (let [first-char (r/next reader)
+        s (->> (if (= first-char \\)
+                 (read-to-char-boundary reader)
+                 (read-to-boundary reader))
+               (str first-char))
         v (r/string->edn s)]
     (node/token-node v s)))
