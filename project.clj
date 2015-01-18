262539a6c71a1904e0cf8147f5cb486002485200
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-01-18 09:22:06 +0100

    Version 0.4.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rewrite-clj "0.4.3"
+(defproject rewrite-clj "0.4.4-SNAPSHOT"
   :description "Comment-/Whitespace-preserving rewriting of EDN documents."
   :url "https://github.com/xsc/rewrite-clj"
   :license {:name "Eclipse Public License"
