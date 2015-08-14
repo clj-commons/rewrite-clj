d9f630cbfca55d3a4dc4642fecafb6cead01b9a4
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-08-14 22:36:18 +0200

    upgrade dependencies.

--- project.clj
+++ project.clj
@@ -6,10 +6,10 @@
             :year 2013
             :key "mit"}
   :repositories  {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.reader "0.9.2"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3" :exclusions [joda-time]]
-                                  [joda-time "2.7"]]
+  :profiles {:dev {:dependencies [[midje "1.7.0" :exclusions [joda-time]]
+                                  [joda-time "2.8.2"]]
                    :plugins [[lein-midje "3.1.3"]
                              [codox "0.8.10"]]
                    :exclusions [org.clojure/clojure]
@@ -17,7 +17,7 @@
                            :defaults {:doc/format :markdown}}}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha5"]]}}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}}
   :aliases {"all" ["with-profile" "dev,1.5:dev,1.6:dev,1.7"]
             "test" ["midje"]
             "test-ancient" ["with-profile" "dev,1.5:dev,1.6:dev,1.7" "midje"]}
