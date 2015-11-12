a5015eee58dbae2f1ae4192e1d931c4586aa7bb6
Jason Felice <jason.m.felice@gmail.com>
2015-11-11 19:09:59 -0500

    Cover more cases

--- test/rewrite_clj/zip/insert_test.clj
+++ test/rewrite_clj/zip/insert_test.clj
@@ -15,22 +15,24 @@
               loc' (?f loc 'x)]
           (base/tag loc) => (base/tag loc')
           (base/root-string loc') => ?s))
-  ?fmt     ?m       ?n   ?f               ?s
-  "[%s]"   m/next   0    insert-right     "[1 2 3 4] x"
-  "[%s]"   m/next   1    insert-right     "[1 x 2 3 4]"
-  "[%s]"   m/next   2    insert-right     "[1 2 x 3 4]"
-  "[%s]"   m/next   3    insert-right     "[1 2 3 x 4]"
-  "[%s]"   m/next   4    insert-right     "[1 2 3 4 x]"
-  "[%s]"   m/next   0    insert-left      "x [1 2 3 4]"
-  "[%s]"   m/next   1    insert-left      "[x 1 2 3 4]"
-  "[%s]"   m/next   2    insert-left      "[1 x 2 3 4]"
-  "[%s]"   m/next   3    insert-left      "[1 2 x 3 4]"
-  "[%s]"   m/next   4    insert-left      "[1 2 3 x 4]"
-  "[%s]"   m/next   0    insert-child     "[x 1 2 3 4]"
-  "[%s]"   m/next   0    append-child     "[1 2 3 4 x]"
-  "[ %s]"  m/next   0    insert-child     "[x 1 2 3 4]"
-  "[%s ]"  m/next   0    append-child     "[1 2 3 4 x]"
-  "[%s]"   z/next   2    insert-right     "[1 x 2 3 4]")
+  ?fmt     ?m         ?n   ?f               ?s
+  "[%s]"   m/next     0    insert-right     "[1 2 3 4] x"
+  "[%s]"   m/next     1    insert-right     "[1 x 2 3 4]"
+  "[%s]"   m/next     2    insert-right     "[1 2 x 3 4]"
+  "[%s]"   m/next     3    insert-right     "[1 2 3 x 4]"
+  "[%s]"   m/next     4    insert-right     "[1 2 3 4 x]"
+  "[%s]"   m/next     0    insert-left      "x [1 2 3 4]"
+  "[%s]"   m/next     1    insert-left      "[x 1 2 3 4]"
+  "[%s]"   m/next     2    insert-left      "[1 x 2 3 4]"
+  "[%s]"   m/next     3    insert-left      "[1 2 x 3 4]"
+  "[%s]"   m/next     4    insert-left      "[1 2 3 x 4]"
+  "[%s]"   m/next     0    insert-child     "[x 1 2 3 4]"
+  "[%s]"   m/next     0    append-child     "[1 2 3 4 x]"
+  "[ %s]"  m/next     0    insert-child     "[x 1 2 3 4]"
+  "[%s ]"  m/next     0    append-child     "[1 2 3 4 x]"
+  "[%s]"   z/next     2    insert-right     "[1 x 2 3 4]"
+  "\n[%s]" z/leftmost 1    insert-left      "x\n[1 2 3 4]"
+  "\n[%s]" z/leftmost 1    insert-right     "\nx [1 2 3 4]")
 
 (tabular
   (fact "about different node types that allow insertion."
