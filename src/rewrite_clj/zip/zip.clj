2ddabaecc901381bc0c43333e2070fd9fa63aca4
Jason Felice <jason.m.felice@gmail.com>
2015-11-16 17:55:07 -0500

    Remove `comment` from zipper impl

--- src/rewrite_clj/zip/zip.clj
+++ src/rewrite_clj/zip/zip.clj
@@ -215,39 +215,3 @@
           [(make-node loc (peek pnodes) rs) 
                      (and ppath (assoc ppath :changed? true))
                      (loc 2)]))))
-  
-(comment
-
-(refer 'zip)
-(def data '[[a * b] + [c * d]])
-
-(right (down (right (right (down dz)))))
-(lefts (right (down (right (right (down dz))))))
-(up (up (right (down (right (right (down dz)))))))
-(path (right (down (right (right (down dz))))))
-
-(-> dz down right right down right)
-(-> dz down right right down right (replace '/) root)
-(-> dz next next (edit str) next next next (replace '/) root)
-(-> dz next next next next next next next next next remove root)
-(-> dz next next next next next next next next next remove (insert-right 'e) root)
-(-> dz next next next next next next next next next remove up (append-child 'e) root)
-
-(end? (-> dz next next next next next next next next next remove next))
-
-(-> dz next remove next remove root)
-
-(loop [loc dz]
-  (if (end? loc)
-    (root loc)
-    (recur (next (if (= '* (node loc)) 
-                   (replace loc '/)
-                   loc)))))
-
-(loop [loc dz]
-  (if (end? loc)
-    (root loc)
-    (recur (next (if (= '* (node loc)) 
-                   (remove loc)
-                   loc)))))
-)
