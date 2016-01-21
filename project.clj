677e81e1d6a2076d9e377ede6b0b5e5234f87242
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-01-21 20:31:14 +0100

    bump minor version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rewrite-clj "0.4.13-SNAPSHOT"
+(defproject rewrite-clj "0.5.0-SNAPSHOT"
   :description "Comment-/Whitespace-preserving rewriting of EDN documents."
   :url "https://github.com/xsc/rewrite-clj"
   :license {:name "MIT License"
