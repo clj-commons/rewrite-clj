ec1e48517c52b536ebe2277c913188ea69588258
TristeFigure <funtain@me.com>
2016-05-22 20:03:44 +0200

    Function names fix

--- src/rewrite_clj/node/seq.clj
+++ src/rewrite_clj/node/seq.clj
@@ -3,10 +3,27 @@
 
 ;; ## Node
 
-(declare
-  seq-fn 
-  seq-format-string
-  seq-wrap-length)
+(defn- seq-fn [^SeqNode n]
+  (case (.tag n)
+    :list     #(apply list %)
+    :vector   vec
+    :set      set
+    :map      #(apply hash-map %)))
+
+(defn- format-string [^SeqNode n]
+  (case (.tag n)
+    :list     "(%s)"
+    :vector   "[%s]"
+    :set      "#{%s}"
+    :map      "{%s}"))
+
+(defn- wrap-length [^SeqNode n]
+  (case (.tag n)
+    (case (.tag n)
+    :list     2
+    :vector   2
+    :set      3
+    :map      2)))
 
 (defrecord SeqNode [tag
                     children]
@@ -17,11 +34,11 @@
   (sexpr [this]
     ((seq-fn this) (node/sexprs children)))
   (length [this]
-    (+ (seq-wrap-length this)
+    (+ (wrap-length this)
        (node/sum-lengths children)))
   (string [this]
     (->> (node/concat-strings children)
-         (format (seq-format-string this))))
+         (format (format-string this))))
 
   node/InnerNode
   (inner? [_]
@@ -31,35 +48,13 @@
   (replace-children [this children']
     (assoc this :children children'))
   (leader-length [this]
-    (dec (seq-wrap-length this)))
+    (dec (wrap-length this)))
 
   Object
   (toString [this]
     (node/string this)))
 
 (node/make-printable! SeqNode)
-
-(defn- seq-fn [^SeqNode n]
-  (case (.tag n)
-    :list     #(apply list %)
-    :vector   vec
-    :set      set
-    :map      #(apply hash-map %)))
-
-(defn- seq-format-string [^SeqNode n]
-  (case (.tag n)
-    :list     "(%s)"
-    :vector   "[%s]"
-    :set      "#{%s}"
-    :map      "{%s}"))
-
-(defn- seq-wrap-length [^SeqNode n]
-  (case (.tag n)
-    (case (.tag n)
-    :list     2
-    :vector   2
-    :set      3
-    :map      2)))
 
 
 ;; ## Constructors
