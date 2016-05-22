7adaee5f142292ab7543d364c5938e2646b92026
TristeFigure <funtain@me.com>
2016-05-22 18:58:43 +0200

    Makes SeqNodes serializable

--- src/rewrite_clj/node/seq.clj
+++ src/rewrite_clj/node/seq.clj
@@ -3,17 +3,18 @@
 
 ;; ## Node
 
+(declare seq-fn)
+
 (defrecord SeqNode [tag
                     format-string
                     wrap-length
-                    seq-fn
                     children]
   node/Node
   (tag [this]
     tag)
   (printable-only? [_] false)
   (sexpr [this]
-    (seq-fn (node/sexprs children)))
+    ((seq-fn this) (node/sexprs children)))
   (length [_]
     (+ wrap-length (node/sum-lengths children)))
   (string [this]
@@ -36,24 +37,32 @@
 
 (node/make-printable! SeqNode)
 
+(defn- seq-fn [^SeqNode n]
+  (case (.tag n)
+    :list     #(apply list %)
+    :vector   vec
+    :set      set
+    :map      #(apply hash-map %)))
+
+
 ;; ## Constructors
 
 (defn list-node
   "Create a node representing an EDN list."
   [children]
-  (->SeqNode :list "(%s)" 2 #(apply list %) children))
+  (->SeqNode :list "(%s)" 2 children))
 
 (defn vector-node
   "Create a node representing an EDN vector."
   [children]
-  (->SeqNode :vector "[%s]" 2 vec children))
+  (->SeqNode :vector "[%s]" 2 children))
 
 (defn set-node
   "Create a node representing an EDN set."
   [children]
-  (->SeqNode :set "#{%s}" 3 set children))
+  (->SeqNode :set "#{%s}" 3 children))
 
 (defn map-node
   "Create a node representing an EDN map."
   [children]
-  (->SeqNode :map "{%s}" 2 #(apply hash-map %) children))
+  (->SeqNode :map "{%s}" 2 children))
