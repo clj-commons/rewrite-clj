640f96d216b86f107fb3ee3bff0ecde4f3a11bdb
Yannick Scherer <yannick.scherer@gmail.com>
2018-05-12 11:43:41 +0200

    Run tests against Clojure 1.9.0 as well

--- project.clj
+++ project.clj
@@ -17,5 +17,5 @@
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}}
-  :aliases {"all" ["with-profile" "dev,1.5:dev,1.6:dev,1.7:dev,1.8"]}
+  :aliases {"all" ["with-profile" "dev,1.5:dev,1.6:dev,1.7:dev,1.8:dev"]}
   :pedantic? :abort)
