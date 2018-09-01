6b359d62ec5bad7d4afe848cd15d116b542c4212
Yannick Scherer <yannick.scherer@gmail.com>
2018-09-01 11:29:52 +0200

    Version 0.6.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rewrite-clj "0.6.1-SNAPSHOT"
+(defproject rewrite-clj "0.6.1"
   :description "Comment-/Whitespace-preserving rewriting of EDN documents."
   :url "https://github.com/xsc/rewrite-clj"
   :license {:name "MIT License"
