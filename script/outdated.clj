6c53f795ceebb8349b8a4d432ba9bdc557d58ccf
lread <lee@dlread.com>
2021-05-14 16:39:42 -0400

    dev: update deps and refine outdated deps check
    The new version of antq has allowed me to exclude a problematic kaocha
    release. Thanks antq!
    
    I've also pinned clojure to version 1.9.0 for pom.xml as this is
    rewrite-clj lib min clojure version. (We don't need to know there are
    updates to clojure 1.9.0 for pom.xml). I did this by asking antq
    to output results in edn, removing pinned version, then using antq
    reporter from bb to present results.

--- script/outdated.clj
+++ script/outdated.clj
@@ -1,14 +1,30 @@
 #!/usr/bin/env bb
 
 (ns outdated
-  (:require [clojure.java.io :as io]
+  (:require [antq.report :as antq-report]
+            [antq.report.table]
+            [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [clojure.string :as string]
             [helper.main :as main]
             [helper.shell :as shell]
             [lread.status-line :as status]))
 
+(def pin-clojure-version-pom "1.9.0")
+
 (defn check-clojure []
   (status/line :head "Checking Clojure deps")
-  (shell/command-no-exit ["clojure" "-M:outdated"]))
+  (if-let [outdated (->> (shell/command-no-exit ["clojure" "-M:outdated"] {:out :string})
+                         :out
+                         string/trim
+                         edn/read-string
+                         (remove #(and (= "pom.xml" (:file %))
+                                       (= "org.clojure/clojure" (:name %))
+                                       (= pin-clojure-version-pom (:version %))))
+                         seq)]
+    (antq-report/reporter outdated {:reporter "table"})
+    (status/line :detail "All Clojure dependencies seem up to date.")))
+
 
 (defn check-nodejs []
   (status/line :head "Checking Node.js deps")
