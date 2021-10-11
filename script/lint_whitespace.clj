018a585b8756e321f9c906c1690e0b5cd863aa37
lread <lee@dlread.com>
2021-10-10 20:55:06 -0400

    lint: only whitespace lint on jdk >= 11
    It seems that camsaul's whitespace-linter requires jdk >= 11.
    
    As a temporary work-around skip whitespace linting if jdk < 11.

--- script/lint_whitespace.clj
+++ script/lint_whitespace.clj
@@ -1,13 +1,17 @@
 #!/usr/bin/env bb
 
 (ns lint-whitespace
-  (:require [helper.main :as main]
+  (:require [helper.jdk :as jdk]
+            [helper.main :as main]
             [helper.shell :as shell]
             [lread.status-line :as status]))
 
 (defn- lint []
   (status/line :head "whitespace: linting")
-  (shell/command "clojure -T:build lint-whitespace"))
+  (let [jdk-version (jdk/version)]
+    (if (< (:major jdk-version) 11)
+      (status/line :warn "Skipping linting, min of JDK 11 required, detected JDK %s" (:version jdk-version))
+      (shell/command "clojure -T:build lint-whitespace"))))
 
 (defn -main [& args]
   (when (main/doc-arg-opt args)
