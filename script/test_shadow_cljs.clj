01dc410552051ac95209589d9da28c34c3e7369a
lread <lee@dlread.com>
2022-04-05 15:14:18 -0400

    internal deps: bump shadow-cljs
    Now explicitly depending on test.check for shadow-cljs test, formerly
    got it only because it happened to be on the shadow-cljs classpath.
    With this newest version this seems to be no longer the case.
    
    The shadow-cljs.edn config file formerly only lived for the life of the
    shadow-cljs test run.
    This meant less chuff in the project root and less duplication of code,
    but also made troubleshooting more difficult.
    Now have an explicit tracked shadow-cljs.edn.

--- script/test_shadow_cljs.clj
+++ script/test_shadow_cljs.clj
@@ -1,30 +1,20 @@
 #!/usr/bin/env bb
 
 (ns test-shadow-cljs
-  (:require [clojure.java.io :as io]
-            [helper.env :as env]
+  (:require [helper.env :as env]
             [helper.main :as main]
             [helper.shell :as shell]
             [lread.status-line :as status]))
 
+;; see also: shadow-cljs.edn
 (def compiled-tests "target/shadow-cljs/node-test.js")
-
-(def shadow-cljs-cfg {:source-paths ["src" "test"]
-                      :builds {:test {:target :node-test
-                                      :output-to compiled-tests
-                                      :compiler-options {:warnings
-                                                         ;; clj-kondo handles deprecation warnings for us
-                                                         {:fn-deprecated false}}}}})
 
 (defn -main [& args]
   (when (main/doc-arg-opt args)
     (status/line :head "testing ClojureScript source with Shadow CLJS, node, optimizations: none")
-    (let [shadow-config-file (io/file "shadow-cljs.edn")]
-      (.deleteOnExit shadow-config-file)
-      (spit shadow-config-file shadow-cljs-cfg)
-      (shell/command (if (= :win (env/get-os)) "npx.cmd" "npx")
-                     "shadow-cljs" "compile" "test")
-      (shell/command "node" compiled-tests)))
+    (shell/command (if (= :win (env/get-os)) "npx.cmd" "npx")
+                   "shadow-cljs" "compile" "test")
+    (shell/command "node" compiled-tests))
   nil)
 
 (main/when-invoked-as-script
