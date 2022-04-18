2c9a91be5b3fa12ed0d71d1b98f7a9728f82b983
lread <lee@dlread.com>
2022-04-18 18:21:05 -0400

    tests: correct test-doc cljs opts generation

--- script/test_doc.clj
+++ script/test_doc.clj
@@ -1,7 +1,8 @@
 #!/usr/bin/env bb
 
 (ns test-doc
-  (:require [helper.main :as main]
+  (:require [babashka.fs :as fs]
+            [helper.main :as main]
             [helper.shell :as shell]
             [lread.status-line :as status]))
 
@@ -19,12 +20,14 @@
 (defn run-cljs-doc-tests []
   (status/line :head "Running code block tests under ClojureScript")
   (let [compile-opts {:warnings {:fn-deprecated false :single-segment-namespace false}}
-        opts-fname "target/cljsbuild/test/doc-tests-opts.edn"]
+        out-dir "target/cljsbuild/doc-tests"
+        opts-fname (fs/file out-dir "doc-tests-opts.edn")]
+    (fs/create-dirs out-dir)
     (spit opts-fname compile-opts)
     (shell/command "clojure" "-M:test-common:test-docs:cljs:cljs-test"
                    "--compile-opts" opts-fname
                    "--dir" "target/test-doc-blocks/test"
-                   "--out" "target/cljsbuild/doc-tests")))
+                   "--out" out-dir)))
 
 (defn -main [& args]
   (when (main/doc-arg-opt args)
