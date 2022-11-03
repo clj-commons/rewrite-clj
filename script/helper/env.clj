41aa84007b6f6a5a9187aeb2e8c62bf6e3e1187c
lread <lee@dlread.com>
2022-11-02 20:01:43 -0400

    Temporary println debugging for macos CI failure

--- script/helper/env.clj
+++ script/helper/env.clj
@@ -8,11 +8,15 @@
   "Asserts minimum version of Clojure version"
   []
   (let [min-version "1.10.1.697"
-        version
-        (->> (shell/command {:out :string} "clojure -Sdescribe")
-             :out
-             edn/read-string
-             :version)]
+        result (shell/command {:out :string} "clojure -Sdescribe")
+        _ (println "ret from clojure -Sdescribe" (pr-str result))
+        version (->> result
+                     :out
+                     edn/read-string
+                     :version)]
+    (when-not version
+      (throw (ex-info "huh, version is nil" {})))
+    (println "parsed version:" (pr-str version))
     (when (< (ver/version-compare version min-version) 0)
       (status/die 1
                   "A  minimum version of Clojure %s required.\nFound version: %s"
