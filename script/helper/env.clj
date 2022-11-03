22d852ccb410e35c7a6235ce05ea98c94a93baf9
lread <lee@dlread.com>
2022-11-02 20:08:55 -0400

    print temp debug to stderr

--- script/helper/env.clj
+++ script/helper/env.clj
@@ -9,14 +9,14 @@
   []
   (let [min-version "1.10.1.697"
         result (shell/command {:out :string} "clojure -Sdescribe")
-        _ (println "ret from clojure -Sdescribe" (pr-str result))
+        _ (.println *err* (str "ret from clojure -Sdescribe: " (pr-str result)))
         version (->> result
                      :out
                      edn/read-string
                      :version)]
     (when-not version
       (throw (ex-info "huh, version is nil" {})))
-    (println "parsed version:" (pr-str version))
+    (.println *err* (str "parsed version: " (pr-str version)))
     (when (< (ver/version-compare version min-version) 0)
       (status/die 1
                   "A  minimum version of Clojure %s required.\nFound version: %s"
