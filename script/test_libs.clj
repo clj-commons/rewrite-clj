b02952c227aa61e26a3375cbf0f1ed7f75820768
Lee Read <lee@dlread.com>
2024-09-19 15:27:11 -0400

    test: temporarily disable refactor-nrepl lib tests (#319)
    * test: temporarily disable refactor-nrepl lib tests
    
    See https://github.com/clojure-emacs/refactor-nrepl/issues/409
    
    * lint

--- script/test_libs.clj
+++ script/test_libs.clj
@@ -214,8 +214,8 @@
 ;;
 ;; refactor-nrepl
 ;;
-
-(defn- refactor-nrepl-patch
+;; uncomment we we re-enable
+#_(defn- refactor-nrepl-patch
   "custom because my generic does not handle ^:inline-dep syntax"
   [{:keys [home-dir rewrite-clj-version]}]
   (status/line :detail "=> Patching deps")
@@ -419,7 +419,8 @@
             :patch-fn deps-edn-v1-patch
             :show-deps-fn cli-deps-tree
             :test-cmds ["clojure -M:test"]}
-           {:name "refactor-nrepl"
+           ;; temporarily disable, see https://github.com/clojure-emacs/refactor-nrepl/issues/409
+           #_{:name "refactor-nrepl"
             :version "3.10.0"
             :platforms [:clj]
             :github-release {:repo "clojure-emacs/refactor-nrepl"
