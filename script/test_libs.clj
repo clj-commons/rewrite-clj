cf6020f7bbea56d73213300ad38719e88e4145ad
lread <lee@dlread.com>
2025-02-07 13:43:43 -0500

    maint: bump tested clojure-lsp

--- script/test_libs.clj
+++ script/test_libs.clj
@@ -343,7 +343,7 @@
                         "bin/test unit"]}
            {:name "clojure-lsp"
             :platforms [:clj]
-            :version "2025.01.22-23.28.23"
+            :version "2025.02.07-16.11.24"
             :github-release {:repo "clojure-lsp/clojure-lsp"}
             :patch-fn clojure-lsp-patch
             :show-deps-fn clojure-lsp-deps
