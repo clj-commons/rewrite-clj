eb2dc193a58f2792974397627bbe681e694aa646
lread <lee@dlread.com>
2025-05-27 12:02:09 -0400

    test libs: bump clojure-lsp to new current

--- script/test_libs.clj
+++ script/test_libs.clj
@@ -327,7 +327,7 @@
                         "bin/test unit"]}
            {:name "clojure-lsp"
             :platforms [:clj]
-            :version "2025.04.23-18.16.46"
+            :version "2025.05.27-13.56.57"
             :github-release {:repo "clojure-lsp/clojure-lsp"}
             :patch-fn clojure-lsp-patch
             :show-deps-fn clojure-lsp-deps
