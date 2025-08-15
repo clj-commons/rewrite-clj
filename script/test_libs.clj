9e6ac11caa76de31dc21ec16cf1df33ba6c13e33
Lee Read <lee@dlread.com>
2025-08-15 14:03:45 +0000

    maintenance (#403)
    * ci: bump github actions checkout
    
    * dev: bump clj-kondo dep
    
    * dev: bump tools build dep
    
    * dev: bump flow storm dep
    
    * test libs: bump clojure-mcp
    
    * ci: lib tests: downgrade gradle for antq tests

--- script/test_libs.clj
+++ script/test_libs.clj
@@ -316,7 +316,7 @@
             :test-cmds ["bb test"]}
            {:name "clojure-mcp"
             :platforms [:clj]
-            :version "0.1.7-alpha"
+            :version "0.1.8-alpha"
             :github-release {:repo "bhauman/clojure-mcp"
                              :via :tag
                              :version-prefix "v"}
