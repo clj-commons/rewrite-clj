154ddaf9f7da77fb3d5a8048deda731922955694
lread <lee@dlread.com>
2025-11-02 09:58:56 -0500

    maint: test-libs: bump cljfmt

--- script/test_libs.clj
+++ script/test_libs.clj
@@ -299,7 +299,7 @@
             :show-deps-fn cli-deps-tree
             :test-cmds ["clojure -T:build ci"]}
            {:name "cljfmt"
-            :version "0.15.1"
+            :version "0.15.3"
             :platforms [:clj :cljs]
             :root "cljfmt"
             :github-release {:repo "weavejester/cljfmt"
