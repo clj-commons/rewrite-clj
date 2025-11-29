6708d48a7b86b0c31b7caa5ae61d76e61ac09d8c
lread <lee@dlread.com>
2025-11-29 11:48:57 -0500

    dev: download-deps task: check args
    Follow conventions for zero-arg tasks.
    Allow for --help.
    Error out on args.

--- script/download_deps.clj
+++ script/download_deps.clj
@@ -1,9 +1,11 @@
 (ns download-deps
-  (:require [babashka.tasks :as t]))
+  (:require [babashka.tasks :as t]
+            [helper.main :as main]))
 
 ;; clojure has a -P command, but to bring down all deps we need to specify all aliases
 ;; bb deps will be brought down just from running bb (which assumedly is how this code is run)
 
-(defn -main [& _args]
-  ;; do all the work from build.clj to avoid repeated JVM launch costs
-  (t/clojure "-T:build download-deps"))
+(defn -main [& args]
+  (when (main/doc-arg-opt args)
+    ;; do all the work from build.clj to avoid repeated JVM launch costs
+    (t/clojure "-T:build download-deps")))
